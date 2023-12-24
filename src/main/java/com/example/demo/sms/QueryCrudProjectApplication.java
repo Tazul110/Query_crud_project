d2877440c43b928179511fcd3d656ba938fa34f4
Tazul islam <it49tazulislam@gmail.com>
2023-12-24 12:43:55 +0600

    first commit

---
+++ src/main/java/com/example/demo/sms/QueryCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryCrudProjectApplication.class, args);
+	}
+
+}
